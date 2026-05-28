5a7a681b4177b40afeab0bfd24c936d7ed5e10d6
Shaul Tolkowsky <stolkowsky@gmail.com>
2026-05-28 15:27:11 +0300

    part 3

--- ex4/utils.py
+++ ex4/utils.py
@@ -1,3 +1,6 @@
+import random
+from collections import defaultdict
+
 import nltk
 from nltk.corpus import dependency_treebank
 
@@ -26,3 +29,44 @@ def feature_function(u, v):
     word_key = ('W', u_word, v_word)
     pos_key  = ('P', u_tag, v_tag)
     return [get_index(word_key), get_index(pos_key)]
+
+
+def gold_edge_features(g):
+    feats = defaultdict(int)
+    for addr, node in g.nodes.items():
+        if addr == 0:
+            continue
+        head = g.nodes[node['head']]
+        for idx in feature_function(head, node):
+            feats[idx] += 1
+    return feats
+
+
+def train(train_sents, infer, n_iterations=2, lr=1.0):
+    weights = defaultdict(float)
+    weights_sum = defaultdict(float)
+    total_steps = 0
+
+    for _ in range(n_iterations):
+        order = list(range(len(train_sents)))
+        random.shuffle(order)
+        for i in order:
+            g = train_sents[i]
+
+            pred_edges = infer(g, weights)  # list of (u_addr, v_addr)
+
+            pred_feats = defaultdict(int)
+            for u_addr, v_addr in pred_edges:
+                for idx in feature_function(g.nodes[u_addr], g.nodes[v_addr]):
+                    pred_feats[idx] += 1
+
+            gold_feats = gold_edge_features(g)
+
+            for idx in set(gold_feats) | set(pred_feats):
+                weights[idx] += lr * (gold_feats[idx] - pred_feats[idx])
+
+            for idx, val in weights.items():
+                weights_sum[idx] += val
+            total_steps += 1
+
+    return {idx: s / total_steps for idx, s in weights_sum.items()}
